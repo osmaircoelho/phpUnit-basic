b7b86d55d2185f6bcc2d19d14360d438a018ea29
Osmair Coelho <osmair.coelho@gmail.com>
2018-02-22 23:04:09 +0000

    - Testing Product Encapsulation

--- tests/ProductTest.php
+++ tests/ProductTest.php
@@ -2,19 +2,39 @@
 
 class ProductTest extends PHPUnit\Framework\TestCase
 {
-    public function testSetName()
+    public function testIfIdIsNull()
     {
         $product = new SON\Model\Product();
-        $product->setName("My product 1");
+        $this->assertNull($product->getId());
+    }
+
+    public function testSetAndGetName()
+    {
+        $product = new SON\Model\Product();
+        $this->assertNull($product->getName());
+        $this->assertInstanceOf(\SON\Model\Product::class, $product->setName('My product 1'));
         $this->assertEquals('My product 1', $product->getName());
     }
 
-    public function testSetPrice()
+    public function testSetAndGetPrice()
     {
         $product = new SON\Model\Product();
-        $product->setPrice(10.10);
-        $this->assertEquals(10.10, $product->getPrice());
+        $this->assertNull($product->getPrice());
+        $this->assertInstanceOf(\SON\Model\Product::class, $product->setPrice(10.11));
+        $this->assertEquals(10.11, $product->getPrice());
+    }
 
-        $this->assertInstanceOf(\SON\Model\Product::class, $product );
+    public function testSetAndGetQuantity()
+    {
+        $product = new SON\Model\Product();
+        $this->assertNull($product->getQuantity());
+        $this->assertInstanceOf(\SON\Model\Product::class, $product->setQuantity(5));
+        $this->assertEquals(5, $product->getQuantity());
+    }
+
+    public function testIfTotalIsNull()
+    {
+        $product = new SON\Model\Product();
+        $this->assertNull($product->getTotal());
     }
 }
