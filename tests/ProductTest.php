e3539b8549d131f23e417064130fe0e50f7a8626
Osmair Coelho <osmair.coelho@gmail.com>
2018-02-23 16:02:43 +0000

    - Creating configuration file

--- tests/ProductTest.php
+++ tests/ProductTest.php
@@ -1,70 +1,47 @@
 <?php
-
 class ProductTest extends PHPUnit\Framework\TestCase
 {
-    public function testIfIdIsNull()
+    private $product;
+    protected function setUp()
     {
-        global $db;
-        echo get_class($db);
-
-        $product = new SON\Model\Product();
-        $this->assertNull($product->getId());
+        $pdo = $this->getMockBuilder(\PDO::class)
+            ->disableOriginalConstructor()->getMock();
+        $this->product = new \SON\Model\Product($pdo);
     }
-
-    /*public function testSetAndGetName()
+    public function testIfIdIsZero()
     {
-        $product = new SON\Model\Product();
-        $this->assertNull($product->getName());
-        $this->assertInstanceOf(\SON\Model\Product::class, $product->setName('My product 1'));
-        $this->assertEquals('My product 1', $product->getName());
+        $this->assertEquals(0,$this->product->getId());
     }
-
-    public function testSetAndGetPrice()
+    public function testIfTotalIsZero()
     {
-        $product = new SON\Model\Product();
-        $this->assertNull($product->getPrice());
-        $this->assertInstanceOf(\SON\Model\Product::class, $product->setPrice(10.11));
-        $this->assertEquals(10.11, $product->getPrice());
+        $this->assertEquals(0.0,$this->product->getTotal());
     }
-
-    public function testSetAndGetQuantity()
-    {
-        $product = new SON\Model\Product();
-        $this->assertNull($product->getQuantity());
-        $this->assertInstanceOf(\SON\Model\Product::class, $product->setQuantity(5));
-        $this->assertEquals(5, $product->getQuantity());
-    }*/
-
-    public function testIfTotalIsNull()
-    {
-        $product = new SON\Model\Product();
-        $this->assertNull($product->getTotal());
-    }
-
     /**
      * @dataProvider collectionData
      */
-
     public function testEncapsulate($property, $expected)
     {
-        $product = new SON\Model\Product();
-
-        $null = $product->{'get'.ucfirst($property)}();
-        $this->assertNull($null);
-
-        $result = $product->{'set'.ucfirst($property)}($expected);
+        $null = $this->product->{'get' . ucfirst($property)}();
+        if(!is_float($expected) && !is_int($expected)) {
+            $this->assertNull($null);
+        }else{
+            if(is_float($expected)){
+                $this->assertEquals(0.0, $null);
+            }else{
+                $this->assertEquals(0, $null);
+            }
+        }
+        $result = $this->product->{'set' . ucfirst($property)}($expected);
         $this->assertInstanceOf(\SON\Model\Product::class, $result);
-
-        $actual = $product->{'get'.ucfirst($property)}();
+        $actual = $this->product->{'get' . ucfirst($property)}();
         $this->assertEquals($expected, $actual);
     }
-
     public function collectionData()
     {
         return [
-            ['name','Product 1'],
-            ['price',10.11],
-            ['quantity',5],
+            ['name', 'Produto 1'],
+            ['price', 10.11],
+            ['quantity', 5],
         ];
     }
 }
